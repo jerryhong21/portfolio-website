03912f4df2a37485203376646640c39091d5b8e5
Jerry Hong <jezzahonga@Jerry-2.local>
2024-02-17 22:28:32 +1100

    added function component section header with styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Intro from "@/components/intro"
+import Intro from "@/components/intro";
 import Header from "@/components/header";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
-  return (
+	return (
 		<main className="flex flex-col items-center px-4">
 			<Header></Header>
 			<Intro></Intro>
+			<SectionDivider></SectionDivider>
 		</main>
-  );
+	);
 }
