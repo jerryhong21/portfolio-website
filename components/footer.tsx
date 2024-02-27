85d47c37b90abc24705797ec57ec4c913e625bb8
Jerry Hong <jh1012037421@gmail.com>
2024-02-27 12:36:15 +1100

    adjusted footer font sizes and styling in footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Footer() {
 	return (
-		<footer className="mt-[7rem] px-4 text-center text-gray-500 ">
-			<small className="mb-2 block text-xs">
+		<footer className="mt-[5rem] px-4 text-center text-gray-500 ">
+			<small className="block text-[0.6rem]">
 				&copy; 2024 Jerry Hong. All rights reserved.
 			</small>
-			<p className="text-xs">
+			<p className="text-[0.6rem]">
 				<span className="font-semibold">About this website:</span> built
 				with React & Next.js (App Router & Server Actions), TypeScript,
 				Tailwind CSS, Framer Motion, React Email & Resend, Vercel
 				hosting.
 			</p>
-			<p className="text-xs underline mt-2">
+			<p className="text-[0.6rem] underline">
 				<a href="https://www.youtube.com/@ByteGrad" target="_blank">
 					Inspired by {"ByteGrad "}
 				</a>{" "}
