0ab3ff8d373b119348df47680555c9052fde78c3
Jerry Hong <jh1012037421@gmail.com>
2024-05-23 02:50:47 +1000

    restored getQuote function

---
+++ actions/getQuote.ts
@@ -0,0 +1,27 @@
+import OpenAI from "openai";
+import { OPENAI_API_KEY } from "../.apiKeys";
+
+const openai = new OpenAI({
+	apiKey: OPENAI_API_KEY,
+	dangerouslyAllowBrowser: true,
+});
+
+export default async function genText() {
+	let completion;
+	try {
+		completion = await openai.chat.completions.create({
+			messages: [
+				{
+					role: "system",
+					content:
+						"Generate an outrageous or atrotious quote, followed by '- Kanye West, probably.'",
+				},
+			],
+			model: "gpt-3.5-turbo",
+		});
+		console.log(completion.choices[0].message.content);
+	} catch (e) {
+		console.error("Error fetching chatGPT message ", e);
+	}
+	if (completion) return completion.choices[0].message.content as string;
+}
