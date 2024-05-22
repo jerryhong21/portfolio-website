e5e21ce047cfa34addb97c732ceba3dc4c896623
Jerry Hong <jh1012037421@gmail.com>
2024-05-23 03:00:13 +1000

    standardised api keys naming

--- actions/getQuote.ts
+++ actions/getQuote.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 // import { OPENAI_API_KEY } from "../.apiKeys";
 
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 	dangerouslyAllowBrowser: true,
 });
 
